f78a679d0392cb383d3de1275521cf86b8cdbd7c
Camilo Tafur <dc.tafur10@uniandes.edu.co>
2023-04-20 17:58:18 -0500

    refactor: update get config method.

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
+from os import environ, path
+from dotenv import load_dotenv
 from datetime import timedelta
-from os import environ
 
+basedir = path.abspath(path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
 
 class Config(object):
     TESTING = False
